38b1fbe9ecbcc4dc6641cb732263891795003587
Rahul5521 <rraahhuullch@gmail.com>
2024-07-04 14:37:45 +0530

    s

--- src/app/allseries/page.js
+++ src/app/allseries/page.js
@@ -1,19 +1,19 @@
-// src/pages/allseries/page.js
+// // src/pages/allseries/page.js
 
-import AllSeriesPage from '../../src/app/allseries/page';
+// import AllSeriesPage from '../../src/app/allseries/page';
 
-const AllSeries = ({ matchData }) => {
-  return (
-    <AllSeriesPage allMatchData={matchData} />
-  );
-};
+// const AllSeries = ({ matchData }) => {
+//   return (
+//     <AllSeriesPage allMatchData={matchData} />
+//   );
+// };
 
-// Fetch your match data here and pass it as props
-AllSeries.getInitialProps = async () => {
-  // Fetch your match data
-  const matchData = await fetchMatchData();
+// // Fetch your match data here and pass it as props
+// AllSeries.getInitialProps = async () => {
+//   // Fetch your match data
+//   const matchData = await fetchMatchData();
 
-  return { matchData };
-};
+//   return { matchData };
+// };
 
-export default AllSeries;
+// export default AllSeries;
