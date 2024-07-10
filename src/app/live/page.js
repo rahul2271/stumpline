7256ec1bc11e1ba928c5cf339642d5f0b6aa0d23
Rahul <121254290+rahul2271@users.noreply.github.com>
2024-07-10 10:49:17 +0530

    Update page.js

--- src/app/live/page.js
+++ src/app/live/page.js
@@ -977,7 +977,7 @@ export default function CardFour() {
                 <div className='text-white font-2xl text-center'>No match found<br/>Check Upcoming Section for more...</div>
               ) : (
                 matchesToShow.map((match, index) => (
-                  <div key={index} className="mx-auto place-content-center bg-white rounded-[20px] p-[20px] flex w-full max-w-7xl flex-col items-center border mb-4 md:flex-row min-[400px]:flex-row">
+                  <div key={index} className="mx-auto place-content-center bg-white rounded-[20px] p-[9px] flex w-full max-w-7xl flex-col items-center border mb-4 md:flex-row min-[300px]:flex-row">
                     <div className='flex flex-col justify-center gap-5 mb-4 md:mb-0 md:mr-4'>
                       <div className="h-[50px] w-[50px]">
                         <img src={match.t1img} alt={match.t1} className="h-[50px] w-[50px] mx-auto rounded-full object-cover" />
@@ -987,20 +987,16 @@ export default function CardFour() {
                       </div>
                     </div>
                     <div className="text-left md:text-left p-4">
-                      <h1 className="inline-flex text-[#FF5E36] items-center text-xl md:text-2xl font-bold">
+                      <h1 className="inline-flex text-[#FF5E36] items-center text-[15px] md:text-2xl font-bold">
                         {match.t1} vs {match.t2}
                       </h1>
-                      <p className="mt-3 text-[18px] font-bold text-gray-900">Scores: {match.t1s} - {match.t2s}</p>
-                      <p className="mt-1 text-lg font-medium text-[#2775f2] animate-pulse">{match.status}</p>
-                      
-                      {/* <p className="mt-1 text-md font-medium text-gray-900">Date and Time: {new Date(match.dateTimeGMT).toLocaleString()}</p> */}
-                      
-                      {/* <p className="mt-1 text-sm font-medium text-gray-900">Venue: {match.venue}</p> */}
-                      {/* <p className="mt-3 text-lg font-medium text-[#2775f2]">{match.status}</p> */}
-                      <p className="mt-2  inline-block rounded-full bg-gray-800 px-3 py-1 text-[12px] font-semibold text-gray-100">Venue: {match.venue}</p>
+                      <p className="mt-3 text-[12px] font-bold text-gray-900">Scores: {match.t1s} - {match.t2s}</p>
+                      <p className="mt-1 text-[10px] font-medium text-[#2775f2] animate-pulse">{match.status}</p>
+
+                      <p className="mt-5  inline-block rounded-full bg-gray-800 px-3 py-1 text-[8px] font-semibold text-gray-100">Venue: {match.venue}</p>
 
                       <div className="mt-1">
-                        <span className="mb-2 mr-2 inline-block rounded-full bg-gray-800 px-3 py-1 text-[10px] font-semibold text-gray-100">
+                        <span className=" mr-2 inline-block rounded-full bg-gray-800 px-3 py-1 text-[10px] font-semibold text-gray-100">
                           {match.series}
                         </span>
                       </div>
@@ -1016,7 +1012,7 @@ export default function CardFour() {
           {activeSection === 'upcoming' && (
             <div className="p-4 rounded-lg shadow-md flex flex-col w-7xl items-center">
               {matchData.map((match, index) => (
-                <div key={index} className="md:mx-auto md:place-content-center bg-white rounded-[20px] p-[20px] flex w-full max-w-7xl flex-col items-center border mb-4 md:flex-row min-[400px]:flex-row">
+                <div key={index} className="md:mx-auto md:place-content-center bg-white rounded-[20px] p-[9px] flex w-full max-w-7xl flex-col items-center border mb-4 md:flex-row min-[300px]:flex-row">
                   <div className="flex flex-col justify-center gap-5 mb-4 md:mb-0 md:mr-4">
                     <div className="h-[50px] w-[50px]">
                       <img src={match.t1img} alt={match.t1} className="h-[50px] w-[50px] mx-auto rounded-full object-cover" />
@@ -1025,18 +1021,17 @@ export default function CardFour() {
                       <img src={match.t2img} alt={match.t2} className="h-[50px] w-[50px] rounded-full object-cover mx-auto" />
                     </div>
                   </div>
-                  <div className="flex-grow pl-0 md:pl-4 min-[400px]:pl-4">
+                  <div className="flex-grow pl-0 md:pl-4 min-[300px]:pl-4">
                     <div className="text-left">
                       <h1 className="inline-flex text-[#FF5E36] items-center text-[15px] font-bold">
                         {match.t1} vs {match.t2}
                       </h1>
-                      {/* <p className="mt-1 text-sm font-medium text-gray-800">Scores: {match.t1s} - {match.t2s}</p> */}
-                      {/* <p className="mt-1 text-sm font-medium text-gray-800">Venue: {match.venue}</p> */}
-                      <p className="mt-1 text-md font-medium text-gray-800">Date and Time: {new Date(match.dateTimeGMT).toLocaleString()}</p>
-                      <p className="mt-1 text-md font-medium text-gray-800">{match.status}</p>
-                      <p className="mt-1  inline-block rounded-full bg-gray-800 px-3 py-1 text-[12px] font-semibold text-gray-100">Venue: {match.venue}</p>
+        
+                      <p className="mt-1 text-[10px] font-bold text-gray-800">Date and Time: {new Date(match.dateTimeGMT).toLocaleString()}</p>
+                      <p className="mt-1 text-[10px] font-medium text-gray-800">{match.status}</p>
+                      <p className="mt-2 inline-block rounded-full bg-gray-800 px-3 py-1 text-[8px] font-semibold text-gray-100">Venue: {match.venue}</p>
 
-                      <div className="mt-2">
+                      <div className="mt-1">
                         <span className="mt-1 mr-2 inline-block rounded-full bg-gray-800 px-3 py-1 text-[10px] font-semibold text-gray-100">
                           {match.series}
                         </span>
@@ -1050,7 +1045,7 @@ export default function CardFour() {
           {activeSection === 'recent' && (
             <div className="p-4 rounded-lg shadow-md flex flex-col items-center">
               {recentMatches.map((match, index) => (
-                <div key={index} className="mx-auto place-content-center bg-white rounded-[20px] p-[20px] flex w-full max-w-7xl flex-col items-center border mb-4 md:flex-row min-[400px]:flex-row">
+                <div key={index} className="mx-auto place-content-center bg-white rounded-[20px] p-[9px] flex w-full max-w-7xl flex-col items-center border mb-4 md:flex-row min-[300px]:flex-row">
                   <div className='flex flex-col justify-center gap-5 mb-4 md:mb-0 md:mr-4'>
                     <div className="h-[50px] w-[50px]">
                       <img src={match.teamInfo[0].img} alt={match.teams[0]} className="h-[50px] w-[50px] mx-auto rounded-full object-cover" />
@@ -1061,18 +1056,16 @@ export default function CardFour() {
                   </div>
                   <div>
                     <div className="text-left md:text-left p-4">
-                      <h1 className="inline-flex text-[#FF5E36] items-center text-[50x] font-bold">
+                      <h1 className="inline-flex text-[#FF5E36] items-center text-sm font-bold">
                         {match.teams[0]} vs {match.teams[1]}
                       </h1>
-                      <p className="mt-1 text-lg font-bold text-gray-800">Scores: {match.score[0]?.r}-{match.score[0]?.w} ({match.score[0]?.o}) - {match.score[1]?.r}-{match.score[1]?.w} ({match.score[1]?.o})</p>
-                      <p className="mt-1 text-sm font-bold text-[#2775f2] animate-pulse">{match.status}</p>
-                      <p className="mt-1 text-sm font-medium text-gray-800">Date and Time: {new Date(match.dateTimeGMT).toLocaleString()}</p>
-                      <p className="mt-2  inline-block rounded-full bg-gray-800 px-3 py-1 text-[12px] font-semibold text-gray-100">Venue: {match.venue}</p>
+                      <p className="mt-1 text-[10px] font-bold text-gray-800">Scores: {match.score[0]?.r}-{match.score[0]?.w} ({match.score[0]?.o}) - {match.score[1]?.r}-{match.score[1]?.w} ({match.score[1]?.o})</p>
+                      <p className="mt-1 text-[10px] font-bold text-[#2775f2] animate-pulse">{match.status}</p>
+                      <p className="mt-1 text-[10px] font-medium text-gray-800">Date and Time: {new Date(match.dateTimeGMT).toLocaleString()}</p>
+                      <p className="mt-2  inline-block rounded-full bg-gray-800 px-3 py-1 text-[8px] font-semibold text-gray-100">Venue: {match.venue}</p>
                     </div>
                   </div>
-                  {/* <div className="md:block hidden h-full w-full md:h-[50px] md:w-[50px]">
-                    <img src={match.teamInfo[1].img} alt={match.teams[1]} className="h-[50px] w-[50px] rounded-full object-cover mx-auto" />
-                  </div> */}
+   
                 </div>
               ))}
             </div>
@@ -1082,5 +1075,6 @@ export default function CardFour() {
     </>
   );
 }
+
 
 
